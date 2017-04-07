6eca406bddb7bd392448b7f3ae86891f9b94dd60
eddy_gg@163.com <eddy_gg@163.com>
2017-04-07 16:29:16 +0800

    py3

--- Basics/IList.py
+++ Basics/IList.py
@@ -1,6 +1,16 @@
 '''
 学习python3的一些简单代码
 '''
-
+import pickle
 # isinstance(item, list)
 # dir(__builtins__)
+
+Istr = 'hi, I am eddie'
+print(Istr.strip('e'))
+
+with open('myData.pickle','wb') as mysavedata:
+    pickle.dump([45,"sadf",'false'], mysavedata)
+with open('myData.pickle','rb') as mystoredata:
+    temlst = pickle.load(mystoredata)
+print(temlst)
+print(mystoredata)
---
+++ Basics/jsonProcess.py
@@ -0,0 +1,20 @@
+import json
+
+
+def JsonParser(file):
+    pdata = dict()
+    with open(file, mode="r", encoding="utf-8") as inf:
+        pdata = json.load(inf)
+    return pdata
+
+
+def test():
+    test_data = JsonParser("E:\Py3Study\Basics\jsonData.json")
+    pdit = {}
+    for pd in test_data:
+        print(pd["batters"]["batter"])
+        for item in pd["batters"]["batter"]:
+            print(item["id"])
+
+if __name__ == '__main__':
+    test()
