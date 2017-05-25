96addc6b8561084d26cefd368537e3ed54ae16a3
eddy_gg@163.com <eddy_gg@163.com>
2017-05-25 10:20:01 +0800

    py3

--- Basics/IList.py
+++ Basics/IList.py
@@ -10,3 +10,35 @@ print('''
 ++++++++++++++
 {}
 '''.format(dc))
+
+# coding:utf-8
+from locust import HttpLocust, TaskSet, task
+import queue
+
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.login()
+
+    def login(self):
+        pass
+
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def profile(self):
+        with self.client.get(
+                "/APIService/v2/enterprise/searchList?appkey=6c8e153ecd964671ac7ae5dc5d4b5e55&keyword=&#x5C0F;&#x7C73;&#x79D1;&#x6280;",
+                catch_response=True) as resp:
+            content = json.loads(resp.text)
+            if (content['status'] == '200'):
+                reps.success()
+                print(resp.text)
+
+
+class WebsiteUser(HttpLocust):
+    host = "http://uatapi.qixin007.com"
+    task_set = UserBehavior
+    min_wait = 500
--- Basics/Idict.py
+++ Basics/Idict.py
@@ -1,13 +1,15 @@
 from collections import OrderedDict
-idict = {"eddie": 345154,
-         "harvey": 13123123}
-try:
-    item = idict["lennie"]
-except KeyError:
-    idict["lennie"] = set()
-else:
-    raise Exception("already exits")
+# idict = {"eddie": 345154,
+#          "harvey": 13123123}
+# try:
+#     item = idict["lennie"]
+# except KeyError:
+#     idict["lennie"] = set()
+# else:
+#     raise Exception("already exits")
+#
+# print(idict)
 
+idict={'p':1,'ed':3}
+idict['o']=10
 print(idict)
-
-idict.__new__()
